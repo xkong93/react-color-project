292dec32f1113e911fb8a981f23eda39eb6d448c
xkong <kong.xu@husky.meu.edu>
2019-07-19 21:28:57 -0700

    fixed route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Palette from './Palette'
 import {Route, Switch} from 'react-router-dom'
 import seedColor from './seedColors'
 import {generatePalette} from './colorHelpers'
 
-function App() {
-    return (
-        <Switch>
-            <Route exact path="/" render={() => <h1> Palette list goes here</h1>}/>
-            <Route exact path="/palette/:id" render={() => <h1>individual route</h1>}/>
-            {/*<div>*/}
-            {/*    <Palette palette={generatePalette(seedColor[3])}/>*/}
-            {/*</div>*/}
-        </Switch>
-    );
+class App extends Component {
+
+    findPalette(id) {
+        return seedColor.find((palette) => palette.id === id)
+    }
+
+    render() {
+        return (
+
+            <Switch>
+                <Route exact path="/" render={() => <h1> Palette list goes here</h1>}/>
+                <Route exact path="/palette/:id"
+                       render={routerProps => (
+                           <Palette palette={generatePalette(
+                               this.findPalette(routerProps.match.params.id))}
+                           />
+                       )}//key point
+                />
+
+            </Switch>
+        );
+    }
+
 }
+
 
 export default App;
