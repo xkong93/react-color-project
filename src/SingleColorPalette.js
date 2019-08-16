0c08193a951a0312c8837e1195842920632982f5
XuanKong <k.xuan29@gmail.com>
2019-08-16 12:16:03 -0700

    add button to single palette

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import NavBar from './NavBar'
 import ColorBox from "./ColorBox";
 import PaletteFooter from "./PaletteFooter"
+import {Link} from "react-router-dom"
 class SingleColorPalette extends Component {
     constructor(props) {
         super(props);
@@ -33,15 +34,18 @@ class SingleColorPalette extends Component {
 
     render() {
         const {format} = this.state
-        const {paletteName, emoji} = this.props.palette
+        const {paletteName, emoji,id} = this.props.palette
         const colorBoxes = this._shades.map(color => (
-            <ColorBox key={color.id} name={color.name} background={color[format]} showLink={false}/>
+            <ColorBox key={color.name} name={color.name} background={color[format]} showLink={false}/>
         ))
         return (
-            <div className='Palette'>
+            <div className='SingleColorPalette Palette'>
                 <NavBar handleChange={this.changeFormat} showingAllColors={false}/>
                 <div className='Palette-colors'>
                     {colorBoxes}
+                    <div className='go-back ColorBox'>
+                        <Link to={`/palette/${id}`} className='back-button'>Go Back</Link>
+                    </div>
                 </div>
                 <PaletteFooter paletteName={paletteName} emoji={emoji}  />
             </div>
