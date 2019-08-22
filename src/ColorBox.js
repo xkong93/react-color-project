c3cb1ff649663b75f03cdd615852c2b76344d9f2
XuanKong <k.xuan29@gmail.com>
2019-08-21 23:04:17 -0700

    adjust color luminance

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 import "./ColoBox.css"
 import {CopyToClipboard} from "react-copy-to-clipboard";
 import {Link} from "react-router-dom"
+import chroma from "chroma-js"
+
 class ColorBox extends Component {
     constructor(props) {
         super(props);
@@ -18,27 +20,29 @@ class ColorBox extends Component {
 
     render() {
         const {name, background,paletteId,id,showLink} = this.props;
-        const {copied} = this.state
+        const {copied} = this.state;
+        const isDarkColor = chroma(background).luminance() <= 0.08 //trick less or equal to 0.05
+        const isLightColor = chroma(background).luminance() >= 0.5 //trick less or equal to 0.05
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div style={{background}} className='ColorBox'>
                     <div style={{background}} className={`copy-overlay ${copied && 'show'}`}/>
                         <div className={`copy-msg ${copied && 'show'}`}>
                             <h1>Copied!</h1>
-                            <p>{this.props.background}</p>
+                            <p className={isLightColor && "dark-text"}>{this.props.background}</p>
                         </div>
 
                         <div className="copy-container">
                             <div className="box-content">
-                                <span>{name}</span>
+                                <span className={isDarkColor && "light-text"}>{name}</span>
                             </div>
-                            <button className="copy-button">Copy</button>
+                            <button className={`copy-button ${isLightColor && "dark-text"} `}>Copy</button>
                         </div>
                     {showLink &&
                         <Link to={`/palette/${paletteId}/${id}`}
                               onClick={(e) => e.stopPropagation()}>
                             {/*//don't fire the button from parent*/}
-                        <span className="see-more">More</span>
+                        <span className={`see-more ${isLightColor && "dark-text"}`}>MORE</span>
                         </Link>}
                     </div>
             </CopyToClipboard>
