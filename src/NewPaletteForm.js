6c543c76fba6d02677a22971b7664ae648334a1d
XuanKong <k.xuan29@gmail.com>
2019-09-06 23:51:27 -0700

    Connect colorPicker to Button

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -1,13 +1,190 @@
-import React, {Component} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import {withStyles} from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import {ChromePicker} from "react-color"
+import Button from '@material-ui/core/Button';
 
-class NewPaletteForm extends Component {
+const drawerWidth = 400;
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginLeft: 12,
+        marginRight: 20,
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: '0 8px',
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing.unit * 3,
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: -drawerWidth,
+    },
+    contentShift: {
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+    },
+});
+
+class PersistentDrawerLeft extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            open: true,
+            currentColor: "teal",
+            colors: [
+                "purple", "#e15764"
+            ]
+        }
+    }
+
+
+    handleDrawerOpen = () => {
+        this.setState({open: true});
+    };
+
+    handleDrawerClose = () => {
+        this.setState({open: false});
+    };
+
+    updateCurrentColor = (newColor) => {
+        this.setState({
+            currentColor: newColor.hex
+        })
+    }
+
+    addNewColor = () => {
+        this.setState({colors: [...this.state.colors, this.state.currentColor]})
+    }
+
     render() {
+        const {classes, theme} = this.props;
+        const {open, colors} = this.state;
+
         return (
-            <div>
-                <h1>new Palette</h1>
+            <div className={classes.root}>
+                <CssBaseline/>
+                <AppBar
+                    position="fixed"
+                    className={classNames(classes.appBar, {
+                        [classes.appBarShift]: open,
+                    })}
+                >
+                    <Toolbar disableGutters={!open}>
+                        <IconButton
+                            color="inherit"
+                            aria-label="Open drawer"
+                            onClick={this.handleDrawerOpen}
+                            className={classNames(classes.menuButton, open && classes.hide)}
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+                        <Typography variant="h6" color="inherit" noWrap>
+                            Persistent drawer
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <Drawer
+                    className={classes.drawer}
+                    variant="persistent"
+                    anchor="left"
+                    open={open}
+                    classes={{
+                        paper: classes.drawerPaper,
+                    }}
+                >
+                    <div className={classes.drawerHeader}>
+                        <IconButton onClick={this.handleDrawerClose}>
+                            {theme.direction === 'ltr' ? <ChevronLeftIcon/> : <ChevronRightIcon/>}
+                        </IconButton>
+                    </div>
+                    <Divider/>
+                    <Typography variant="h4">Design your palette</Typography>
+                    <div>
+                        <Button variant="contained" color="secondary">Clear Palette</Button>
+                        <Button variant="contained" color="primary">Random Color</Button>
+                    </div>
+                    <ChromePicker color={this.state.currentColor} onChangeComplete={this.updateCurrentColor}/>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        style={{backgroundColor: this.state.currentColor}}
+                        onClick={this.addNewColor}
+                    >
+                        Add Color onClick
+                    </Button>
+
+                </Drawer>
+                <main
+                    className={classNames(classes.content, {
+                        [classes.contentShift]: open,
+                    })}
+                >
+                    <div className={classes.drawerHeader}/>
+                    <ul>
+                        {colors.map(color =>
+                            <li style={{backgroundColor:color}}>
+                                {color}
+                            </li>
+                        )}
+                    </ul>
+                </main>
             </div>
         );
     }
 }
 
-export default NewPaletteForm;
+PersistentDrawerLeft.propTypes = {
+    classes: PropTypes.object.isRequired,
+    theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, {withTheme: true})(PersistentDrawerLeft);
